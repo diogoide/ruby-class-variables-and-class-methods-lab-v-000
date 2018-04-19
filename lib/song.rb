80f6c3af3596795e7c10398825e3cc2a17ede8b5
Diogo Ide <diogoide@gmail.com>
2018-04-19 23:29:21 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,32 @@
+class Song
+  attr_accessor :name,:artist,:genre
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(name,artist,genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count.uniq!
+  end
+
+  def self.artists
+    @@artists.uniq!
+  end
+
+  def self.genres
+    @@genres.uniq!
+  end
+
+
+  end
+
+end
+end
